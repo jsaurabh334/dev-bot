d54b8ad233d745b83846000459710402a641a2c8
jsaurabh334 <jsaurabh334@gmail.com>
2024-03-11 16:06:18 +0530

    testing

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -34,6 +34,7 @@ export async function POST(
 
     const response = await replicate.run(
       "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      
       {
         input: {
           prompt_a: prompt
