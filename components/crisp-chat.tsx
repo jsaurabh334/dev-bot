9bbee8f96ba935b5ec13f1ffb4f4adbf589338d7
jsaurabh334 <jsaurabh334@gmail.com>
2024-03-12 09:49:07 +0530

    test

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("80e425b3-389b-45e4-b9bc-9a1eedea4555");
   }, []);
 
   return null;
